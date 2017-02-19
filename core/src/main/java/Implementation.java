efbe5c022f5bb28fe2fe15545f4356f7ee95617f
SirWindfield <svenforgithub@gmail.com>
2017-02-19 23:35:57 +0100

    Added readme, fixed target errors

--- core/src/main/java/Implementation.java
+++ core/src/main/java/Implementation.java
@@ -8,4 +8,9 @@ public class Implementation implements Interface {
     public static void main(String[] args) {
         System.out.println(new Implementation().getString());
     }
+
+    @Override
+    public String toString() {
+        return getString();
+    }
 }
---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/env/bash
+# this script basically auto detects any remote_jar dependencies and fetches them into the cache
